b0cf684900bbdcdc8d2b77ab875d7bb769177647
Shankie <andrew@properdesign.co.uk>
2016-11-30 21:44:06 +0000

    test

--- server.js
+++ server.js
@@ -39,10 +39,10 @@ slapp.message('help', ['mention', 'direct_message'], (msg) => {
 slapp
   .message('^.*', ['direct_mention', 'direct_message'], (msg, text) => {
     msg
-      .say('got it');
-      console.log(msg);
-      console.log(text);
-      console.log(chrono.parseDate(msg));
+      .say(chrono.parseDate(msg.body.event.text));
+      // console.log(msg);
+      // console.log(text);
+      // console.log(chrono.parseDate(msg.body.event.text));
       // sends next event from user to this route, passing along state
       // .route('how-are-you', { greeting: text })
   })
