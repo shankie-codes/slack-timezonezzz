87adb04a9c5867b48a6c6ef20418d33c2a814199
Shankie <andrew@properdesign.co.uk>
2016-12-24 12:22:25 +0000

    Testing

--- server.js
+++ server.js
@@ -52,6 +52,8 @@ slapp
       // Get the Unix time and convert to seconds
       let parsedTime = parsedDate.getTime() / 1000;
 
+      console.log(msg.body.event);
+      
       // Get the users in this channel
       request
       .get(`https://slack.com/api/channels.info?channel=${msg.body.event.channel_id}&token=${msg.meta.bot_token}`)
