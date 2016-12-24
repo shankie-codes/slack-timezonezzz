23ca4b9a0f559d7efd7548420aec208388f6377d
Shankie <andrew@properdesign.co.uk>
2016-12-24 12:18:16 +0000

    Oopsy

--- server.js
+++ server.js
@@ -51,10 +51,10 @@ slapp
     if(parsedDate){
       // Get the Unix time and convert to seconds
       let parsedTime = parsedDate.getTime() / 1000;
-      console.log(`channel is ${msg.body.event.item.channel}`)
+
       // Get the users in this channel
       request
-      .get(`https://slack.com/api/channels.info?channel=${msg.body.event.item.channel}&token=${msg.meta.bot_token}`)
+      .get(`https://slack.com/api/channels.info?channel=${msg.body.event.channel}&token=${msg.meta.bot_token}`)
       .end((err, res) =>{
         const usersInChannel = res.channel.members;
         // Get the timezeones that are in use in this channel
